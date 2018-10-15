c9c90f013d055e89a1fa0029a1d8ed91f1f84e8d
Lotus <cmadisonne@gmail.com>
2018-10-15 11:09:25 -0500

    New Tests

--- pages/tests.py
+++ pages/tests.py
@@ -29,6 +29,14 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertNotContains(
             response, 'Hi there! I should not be on the page.')
+    #     /////////////////////////////////////////////////////////////
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_about_view_url_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEquals(response.status_code, 200)
 
     def test_about_view_uses_correct_template(self):
         response = self.client.get(reverse('about'))
@@ -38,3 +46,8 @@ class HomePageTests(SimpleTestCase):
     def test_about_home_page_contains_correct_html(self):
         response = self.client.get('/about/')
         self.assertContains(response, '<h1>About page</h1>')
+
+    def test_about_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/about/')
+        self.assertNotContains(
+            response, 'This is a fake about page.')
